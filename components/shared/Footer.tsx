079f23c6714a94d463dde4080f70fbd713d764be
fatima ezzahra <mikassaakerman1977@gmail.com>
2024-05-28 17:22:47 -0700

    project & clerk setup

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+      <div  className='flex-center flex wrapper flex-between flex-col gap-4 p-5 
+       text-center sm:flex-row '>
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2024 Evently. All rights resrved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes:[
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+  ],
+  ignoredRoutes:[
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+    '/assets/images/logo.svg',
+    '/favicon.ico',
+    '/assets/images/dotted-pattern.png'
+
+  ]
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
